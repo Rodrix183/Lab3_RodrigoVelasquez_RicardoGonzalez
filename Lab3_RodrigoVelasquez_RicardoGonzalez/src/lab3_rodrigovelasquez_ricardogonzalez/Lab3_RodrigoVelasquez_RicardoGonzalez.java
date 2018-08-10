572b989847ae2b47bb407cc68cdb328a0509e0f7
rodrix183 <rodrix183@hotmail.com>
2018-08-10 13:35:28 -0600

    Creacion lab

---
+++ Lab3_RodrigoVelasquez_RicardoGonzalez/src/lab3_rodrigovelasquez_ricardogonzalez/Lab3_RodrigoVelasquez_RicardoGonzalez.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_rodrigovelasquez_ricardogonzalez;
+
+/**
+ *
+ * @author rodri
+ */
+public class Lab3_RodrigoVelasquez_RicardoGonzalez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
