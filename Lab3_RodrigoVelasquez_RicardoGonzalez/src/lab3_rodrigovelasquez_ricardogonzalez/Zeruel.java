02fb64a77417982e59c65ad3d1a457ee1c73a9ce
RicardoGonzalez27 <wolfdarklightjaeger@gmail.com>
2018-08-10 14:56:43 -0600

    Mas clases

---
+++ Lab3_RodrigoVelasquez_RicardoGonzalez/src/lab3_rodrigovelasquez_ricardogonzalez/Zeruel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_rodrigovelasquez_ricardogonzalez;
+
+/**
+ *
+ * @author Richard
+ */
+public class Zeruel extends Angel{
+    
+    public void Movimiento(){
+        
+    }
+    
+    @Override
+    public void Atacar() {
+
+    }
+    
+}
