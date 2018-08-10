a8d01fc26157266cc7985c3e9391fd6228b3e27c
RicardoGonzalez27 <wolfdarklightjaeger@gmail.com>
2018-08-10 17:19:00 -0600

    Correccion de Movimiento

--- Lab3_RodrigoVelasquez_RicardoGonzalez/src/lab3_rodrigovelasquez_ricardogonzalez/EVA00.java
+++ Lab3_RodrigoVelasquez_RicardoGonzalez/src/lab3_rodrigovelasquez_ricardogonzalez/EVA00.java
@@ -11,7 +11,16 @@ package lab3_rodrigovelasquez_ricardogonzalez;
  */
 public class EVA00 extends EVA {
     
-    public boolean Movimiento(int posx, int posy, int movx, int movy) {
+    public boolean Movimiento(String [][] tokyo3, int posx, int posy, int movx, int movy) {
+        if (movx > 9 || movx < 0) {
+            return false;
+        }
+        if (movy > 9 || movy < 0) {
+            return false;
+        }
+        if (tokyo3[movx][movy].equals("["+"PM "+"] ") || tokyo3[movx][movy].equals("["+"EV0"+"] ") || tokyo3[movx][movy].equals("["+"EV1"+"] ") || tokyo3[movx][movy].equals("["+"EV2"+"] ") || tokyo3[movx][movy].equals("["+"-S-"+"] ") || tokyo3[movx][movy].equals("["+"-R-"+"] ") || tokyo3[movx][movy].equals("["+"-Z-"+"] ") || tokyo3[movx][movy].equals("["+"---"+"] ")) {
+            return false;
+        }
         if ((movx == posx -1 || movx == posx +1)) {
             if (movy == posy -1 || movy == posy +1) {
                 return true;
@@ -20,8 +29,7 @@ public class EVA00 extends EVA {
         return false;
     }
     
-    @Override
-    public void Atacar() {
+    public void Atacar(int [][] tokyo3, int posx, int posy, int movx, int movy) {
         
     }
 }
--- Lab3_RodrigoVelasquez_RicardoGonzalez/src/lab3_rodrigovelasquez_ricardogonzalez/Zeruel.java
+++ Lab3_RodrigoVelasquez_RicardoGonzalez/src/lab3_rodrigovelasquez_ricardogonzalez/Zeruel.java
@@ -9,18 +9,27 @@ package lab3_rodrigovelasquez_ricardogonzalez;
  *
  * @author Richard
  */
-public class Zeruel extends Angel{
-    
-    public boolean Movimiento(int posx, int posy, int movx, int movy){
-        if (movx == posx && movy == posy-2) {
+public class Zeruel extends Angel {
+
+    public boolean Movimiento(String[][] tokyo3, int posx, int posy, int movx, int movy) {
+        if (movx > 9 || movx < 0) {
+            return false;
+        }
+        if (movy > 9 || movy < 0) {
+            return false;
+        }
+        if (tokyo3[movx][movy].equals("[" + "PM " + "] ") || tokyo3[movx][movy].equals("[" + "EV0" + "] ") || tokyo3[movx][movy].equals("[" + "EV1" + "] ") || tokyo3[movx][movy].equals("[" + "EV2" + "] ") || tokyo3[movx][movy].equals("[" + "-S-" + "] ") || tokyo3[movx][movy].equals("[" + "-R-" + "] ") || tokyo3[movx][movy].equals("[" + "-Z-" + "] ") || tokyo3[movx][movy].equals("[" + "---" + "] ")) {
+            return false;
+        }
+        if (movx == posx && movy == posy - 2) {
             return true;
         }
         return false;
     }
-    
+
     @Override
     public void Atacar() {
 
     }
-    
+
 }
