6b7f873bf2ec4af49c95975cd5bf3d6f994f8fbf
rodrix183 <rodrix183@hotmail.com>
2018-08-10 14:33:26 -0600

    Clases

--- Lab3_RodrigoVelasquez_RicardoGonzalez/src/lab3_rodrigovelasquez_ricardogonzalez/Lab3_RodrigoVelasquez_RicardoGonzalez.java
+++ Lab3_RodrigoVelasquez_RicardoGonzalez/src/lab3_rodrigovelasquez_ricardogonzalez/Lab3_RodrigoVelasquez_RicardoGonzalez.java
@@ -3,16 +3,16 @@ package lab3_rodrigovelasquez_ricardogonzalez;
 import java.util.Scanner;
 
 public class Lab3_RodrigoVelasquez_RicardoGonzalez {
-public static Scanner sc = new Scanner(System.in);
-public static String [][] tokyo3 = new String[10][10];
-//Evas
-public static String PRODMASA = "PM", EVA_00="EV0", EVA_01="EV1", EVA_02="EV2";
-//Angeles
-public static String SACHIEL = "S", RAMIEL="R", ZERUEL="Z";
 
+    public static Scanner sc = new Scanner(System.in);
+    public static String[][] tokyo3 = new String[10][10];
+//Evas
+    public static String PRODMASA = "PM", EVA_00 = "EV0", EVA_01 = "EV1", EVA_02 = "EV2";
+//Angeles
+    public static String SACHIEL = "S", RAMIEL = "R", ZERUEL = "Z";
 
     public static void main(String[] args) {
-        int resp  = 0;
+        int resp = 0;
         while (resp != 5) {
             try {
                 System.out.println("1) Lista de EVAs\n"
@@ -22,18 +22,62 @@ public static String SACHIEL = "S", RAMIEL="R", ZERUEL="Z";
                         + "5) Salir\n"
                         + "Ingrese opcion");
                 resp = sc.nextInt();
-                
-                switch(resp){
+
+                switch (resp) {
                     case 1:
+
                         break;
                     case 2:
+
                         break;
                     case 3:
+
                         break;
                     case 4:
                         for (int i = 0; i < 10; i++) {
                             for (int j = 0; j < 10; j++) {
-                                tokyo3[i][j] = "- ";
+                                if (i == 0 && j == 2) {//aqui van los Angeles
+                                    tokyo3[i][j] = "["+RAMIEL+"] "; //ramiel
+                                }else if (i == 0 && j == 7) {
+                                    tokyo3[i][j] = "["+RAMIEL+"] ";
+                                }else if (i == 1 && j == 1) {//zeruel
+                                    tokyo3[i][j] = "["+ZERUEL+"] ";
+                                }else if (i == 1 && j == 8) {
+                                    tokyo3[i][j] = "["+ZERUEL+"] ";
+                                }else if (i == 3 && j == 2) {
+                                    tokyo3[i][j] = "["+ZERUEL+"] ";
+                                }else if (i == 3 && j == 7) {
+                                    tokyo3[i][j] = "["+ZERUEL+"] ";
+                                }else if (i == 1 && j == 4) { //sachiel
+                                    tokyo3[i][j] = "["+SACHIEL+"] ";
+                                }else if (i == 1 && j == 5) {
+                                    tokyo3[i][j] = "["+SACHIEL+"] ";
+                                }
+                                
+                                //aqui los evas
+                                else if (i == 9 && j == 0) {//pm
+                                    tokyo3[i][j] = "["+PRODMASA+"] ";
+                                } else if (i == 9 && j == 9) {
+                                    tokyo3[i][j] = "["+PRODMASA+"] ";
+                                } else if (i == 9 && j == 2) {
+                                    tokyo3[i][j] = "["+PRODMASA+"] ";
+                                } else if (i == 9 && j == 7) {
+                                    tokyo3[i][j] = "["+PRODMASA+"] ";
+                                } else if (i == 9 && j == 3) { //eva 00
+                                    tokyo3[i][j] = "["+EVA_00+"] "; 
+                                } else if (i == 9 && j == 6) {
+                                    tokyo3[i][j] = "["+EVA_00+"] ";
+                                } else if (i == 9 && j == 4) { //eva 01
+                                    tokyo3[i][j] = "["+EVA_01+"] ";
+                                } else if (i == 9 && j == 5) { //eva 02
+                                    tokyo3[i][j] = "["+EVA_02+"] ";
+                                }
+                                
+                                
+                                else{
+                                    tokyo3[i][j] = "["+"-"+"] ";
+                                }
+                                
                                 System.out.print(tokyo3[i][j]);
                             }
                             System.out.println("");
@@ -42,11 +86,10 @@ public static String SACHIEL = "S", RAMIEL="R", ZERUEL="Z";
                     default:
                         resp = 5;
                 }
-                
+
             } catch (Exception e) {
                 System.out.println("Opcion no valida! ");
             }
-           
 
         }
     }
