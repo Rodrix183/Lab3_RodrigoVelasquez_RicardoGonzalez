659427bf492f6814ec337d81a5498217060ce8ad
rodrix183 <rodrix183@hotmail.com>
2018-08-10 15:48:20 -0600

    Posiciones de cada Ser

--- Lab3_RodrigoVelasquez_RicardoGonzalez/src/lab3_rodrigovelasquez_ricardogonzalez/Lab3_RodrigoVelasquez_RicardoGonzalez.java
+++ Lab3_RodrigoVelasquez_RicardoGonzalez/src/lab3_rodrigovelasquez_ricardogonzalez/Lab3_RodrigoVelasquez_RicardoGonzalez.java
@@ -12,9 +12,17 @@ public class Lab3_RodrigoVelasquez_RicardoGonzalez {
     public static ArrayList<EVA> listaEVA = new ArrayList<>();
     public static ArrayList<Angel> listaAngeles = new ArrayList<>();
     public static ArrayList<Piloto> listaPilotos = new ArrayList<>();
-//Evas
+
+    //posiciones angeles
+    public static int xS1 = 1, xS2 = 1, xR1 = 0, xR2 = 0, xZ1 = 1, xZ2 = 1, xZ3 = 3, xZ4 = 3, yS1 = 4, yS2 = 5, yR1 = 2, yR2 = 7, yZ1 = 1, yZ2 = 8, yZ3 = 2, yZ4 = 7;
+
+    //posiciones EVAS
+    public static int xPM1 = 9, xPM2 = 9, xPM3 = 9, xPM4 = 9, x00p = 9, x00s = 9, x01 = 9, x02 = 9, yPM1 = 0, yPM2 = 2, yPM3 = 7, yPM4 = 9, y00p = 3, y00s = 6, y01 = 4, y02 = 5;
+
+    //Evas
     public static String PRODMASA = "PM ", EVA_00 = "EV0", EVA_01 = "EV1", EVA_02 = "EV2";
-//Angeles
+
+    //Angeles
     public static String SACHIEL = "-S-", RAMIEL = "-R-", ZERUEL = "-Z-";
 
     public static void main(String[] args) {
@@ -41,21 +49,21 @@ public class Lab3_RodrigoVelasquez_RicardoGonzalez {
                 listaPilotos.add(new Piloto("Rodrigo", 20, "Max", "Encargado del EVA", "IST", 70.43, "EVA-00"));
                 listaPilotos.add(new Piloto("Nelson", 19, "Avila Crusoe el Rio", "Encargado del EVA", "DelCampo", 84.34, "EVA-01"));
                 listaPilotos.add(new Piloto("Shinji", 15, "Dr. Hikari", "Encargado del EVA", "Americana", 84.34, "EVA-02"));
-                
+
                 //EVAS
                 //produccion en masa
                 listaEVA.add(new EVA(Color.yellow, 1998, 3.4, "Salvaje", 2, null, 99, 65));
                 listaEVA.add(new EVA(Color.yellow, 1998, 3.4, "Hostil", 2, null, 99, 65));
                 listaEVA.add(new EVA(Color.yellow, 1998, 3.4, "Salvaje", 2, null, 99, 65));
                 listaEVA.add(new EVA(Color.yellow, 1998, 3.4, "Nuevo", 2, null, 99, 65));
-                
+
                 //eva00
                 listaEVA.add(new EVA(Color.yellow, 1998, 17, "Pura", 2, listaPilotos.get(0), 99, 65));
                 listaEVA.add(new EVA(Color.yellow, 1998, 17, "Corrupta", 2, listaPilotos.get(1), 99, 65));
-                
+
                 //eva01
                 listaEVA.add(new EVA(Color.yellow, 1998, 13, "Pura", 2, listaPilotos.get(2), 99, 65));
-                
+
                 //eva02
                 listaEVA.add(new EVA(Color.yellow, 1998, 11, "Nueva", 2, listaPilotos.get(3), 99, 65));
                 System.out.println("1) Lista de EVAs\n"
@@ -98,38 +106,38 @@ public class Lab3_RodrigoVelasquez_RicardoGonzalez {
         String[][] matriz = new String[10][10];
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (i == 0 && j == 2) {//aqui van los Angeles
+                if (i == xR1 && j == yR1) {//aqui van los Angeles
                     tokyo3[i][j] = "[" + RAMIEL + "] "; //ramiel
-                } else if (i == 0 && j == 7) {
+                } else if (i == xR2 && j == yR2) {
                     tokyo3[i][j] = "[" + RAMIEL + "] ";
-                } else if (i == 1 && j == 1) {//zeruel
+                } else if (i == xZ1 && j == yZ1) {//zeruel #1
                     tokyo3[i][j] = "[" + ZERUEL + "] ";
-                } else if (i == 1 && j == 8) {
+                } else if (i == xZ2 && j == yZ2) { //#2
                     tokyo3[i][j] = "[" + ZERUEL + "] ";
-                } else if (i == 3 && j == 2) {
+                } else if (i == xZ3 && j == yZ3) { //#3
                     tokyo3[i][j] = "[" + ZERUEL + "] ";
-                } else if (i == 3 && j == 7) {
+                } else if (i == xZ4 && j == yZ4) { //#4
                     tokyo3[i][j] = "[" + ZERUEL + "] ";
-                } else if (i == 1 && j == 4) { //sachiel
+                } else if (i == xS1 && j == yS1) { //sachiel
                     tokyo3[i][j] = "[" + SACHIEL + "] ";
-                } else if (i == 1 && j == 5) {
+                } else if (i == xS2 && j == yS2) {
                     tokyo3[i][j] = "[" + SACHIEL + "] ";
                 } //aqui los evas
-                else if (i == 9 && j == 0) {//pm
+                else if (i == xPM1 && j == yPM1) {//pm
                     tokyo3[i][j] = "[" + PRODMASA + "] ";
-                } else if (i == 9 && j == 9) {
+                } else if (i == xPM2 && j == yPM2) {
                     tokyo3[i][j] = "[" + PRODMASA + "] ";
-                } else if (i == 9 && j == 2) {
+                } else if (i == xPM2 && j == yPM3) {
                     tokyo3[i][j] = "[" + PRODMASA + "] ";
-                } else if (i == 9 && j == 7) {
+                } else if (i == xPM4 && j == yPM4) {
                     tokyo3[i][j] = "[" + PRODMASA + "] ";
-                } else if (i == 9 && j == 3) { //eva 00
+                } else if (i == x00p && j == y00p) { //eva 00
                     tokyo3[i][j] = "[" + EVA_00 + "] ";
-                } else if (i == 9 && j == 6) {
+                } else if (i == x00s && j == y00s) {
                     tokyo3[i][j] = "[" + EVA_00 + "] ";
-                } else if (i == 9 && j == 4) { //eva 01
+                } else if (i == x01 && j == y01) { //eva 01
                     tokyo3[i][j] = "[" + EVA_01 + "] ";
-                } else if (i == 9 && j == 5) { //eva 02
+                } else if (i == x02 && j == y02) { //eva 02
                     tokyo3[i][j] = "[" + EVA_02 + "] ";
                 } else {
                     tokyo3[i][j] = "[" + "---" + "] ";
